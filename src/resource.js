54defd122e36ebef6f5e4a112332ba098d628df4
Taiki Arakawa <at.090313@gmail.com>
2016-08-26 09:27:10 +0900

    エフェクト設定

---
+++ src/resource.js
@@ -0,0 +1,29 @@
+var res = {
+  chara_princessselect_10: "res/char/chara_princessselect_10.png",
+  chara_princessselect_11: "res/char/chara_princessselect_11.png",
+  chara_princessselect_12: "res/char/chara_princessselect_12.png",
+  ss_BattleScene_bg1: "res/field/ss_BattleScene_bg1.jpg",
+  ss_BattleScene_bg2: "res/field/ss_BattleScene_bg2.jpg",
+  ss_BattleScene_bg3: "res/field/ss_BattleScene_bg3.jpg",
+  chara_enemy_1: "res/char/chara_enemy_1.png",
+  chara_enemy_2: "res/char/chara_enemy_2.png",
+  chara_enemy_3: "res/char/chara_enemy_3.png",
+  chara_enemy_4: "res/char/chara_enemy_4.png",
+  chara_enemy_5: "res/char/chara_enemy_5.png",
+  chara_enemy_6: "res/char/chara_enemy_6.png",
+  fire_particl1: "res/effect/fire_texture1.plist",
+  fire_particl2: "res/effect/fire_texture2.plist",
+  fire_particl3: "res/effect/fire_texture3.plist",
+  laser_particl1: "res/effect/laser_texture1.plist",
+  laser_particl2: "res/effect/laser_texture2.plist",
+  laser_particl3: "res/effect/laser_texture3.plist",
+  laser_particl4: "res/effect/laser_texture4.plist",
+  exp_particl1: "res/effect/exp_texture1.plist",
+  exp_particl2: "res/effect/exp_texture2.plist",
+  exp_particl3: "res/effect/exp_texture3.plist"
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
